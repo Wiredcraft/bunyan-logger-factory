4858ba1155166c4b07d4c3e8ba3eaee756721599
Xavier Zhou <xiayezhou@gmail.com>
2019-02-02 23:24:16 +0800

    chore: init

---
+++ index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const bsyslog = require('bunyan-syslog');
+const bunyan = require('bunyan');
+
+const getStream = config => {
+  let stream;
+  switch (config.logStream.toUpperCase()) {
+    case 'FILE':
+      stream = { path: config.logPath || './logs/app.log' };
+      break;
+    case 'SYSLOG':
+      stream = {
+        type: 'raw',
+        stream: bsyslog.createBunyanStream({
+          host: config.logHost,
+          port: parseInt(config.logPort, 10),
+          facility: bsyslog.facility.local0,
+          type: config.logProto
+        })
+      };
+      break;
+    case 'STDOUT':
+    default:
+      stream = { stream: process.stdout };
+  }
+  return stream;
+};
+
+const noStackErrSerializers = function(err) {
+  return { message: err.message, name: err.name, code: err.code };
+};
+
+exports.init = function(config) {
+  return bunyan.createLogger({
+    name: config.logName,
+    streams: [getStream(config)],
+    serializers: {
+      // customize err serializer coz buyan std err serializer doesn't work without err.stack
+      err:
+        process.env.NODE_ENV === 'production'
+          ? noStackErrSerializers
+          : bunyan.stdSerializers.err,
+      req: bunyan.stdSerializers.req,
+      res: bunyan.stdSerializers.res
+    }
+  });
+};
+
+exports.getStream = getStream;
---
+++ test/stream.test.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const chai = require('chai');
+chai.should();
+const loggerFactory = require('../index');
+describe('getStream', () => {
+  it('should work with stdout', () => {
+    const stm = loggerFactory.getStream({
+      logStream: 'STDOUT'
+    });
+    stm.stream.should.be.eql(process.stdout);
+  });
+  it('logstream should support lowercase', () => {
+    const stm = loggerFactory.getStream({
+      logStream: 'stdout'
+    });
+    stm.stream.should.be.eql(process.stdout);
+  });
+  it('should work with file', () => {
+    const stm = loggerFactory.getStream({
+      logStream: 'FILE',
+      logPath: './foo/bar'
+    });
+    stm.should.be.eql({ path: './foo/bar' });
+  });
+  it('should work with syslog', () => {
+    const stm = loggerFactory.getStream({
+      logStream: 'SYSLOG',
+      logHost: 'localhost',
+      logPort: 514,
+      logProto: 'tcp'
+    });
+    stm.type.should.be.equal('raw');
+  });
+});
